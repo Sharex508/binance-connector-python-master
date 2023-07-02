a37d45615828ac4b47312414af2179fc0ff7a426
Sharex508 <47905309+Sharex508@users.noreply.github.com>
2023-07-02 22:34:02 +0530

    Create dbcrea.py

---
+++ dbcrea.py
@@ -0,0 +1,22 @@
+import psycopg2
+from psycopg2 import Error
+import psycopg2
+from psycopg2 import Error
+  
+conn = psycopg2.connect(
+   database="postgres", user='postgres', password='harsha508', host='localhost', port= '5432'
+)
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Preparing query to create a database  
+sql = '''CREATE database crypto''';
+
+#Creating a database
+cursor.execute(sql)
+print("Database created successfully........")
+
+#Closing the connection
+conn.close()
